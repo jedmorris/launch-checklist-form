41f6d993ab624de3eb58fd0e3103baafc196024e
Jed Morris <jemorris110@gmail.com>
2020-05-27 10:17:31 -0700

    added prevent.default for fuelLevel & cargoMass

--- script.js
+++ script.js
@@ -3,15 +3,14 @@
 
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-   form.addEventListener("formSubmit", function(event) {
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
-      // let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      // let cargoMass = document.querySelector("input[name=cargoMass]");
-      if (pilotNameInput.value === "" || copilotName.value === "") {
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (pilotNameInput.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
       alert("All fields are required!");
-      // event.preventDefault();
-      // }
       }
      });
    });
